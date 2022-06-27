59109445af849fb652827e336b79cf00dbf69bfe
shubham-botsync <shubham.k@botsync.co>
2022-06-28 01:57:01 +0530

    added read_ground_truth_poses function

--- include/visual_odom.hpp
+++ include/visual_odom.hpp
@@ -3,9 +3,16 @@
 
 
 #include <iostream>
+#include <sstream>
 
 //*** opencv headers
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core/persistence.hpp>
+
 
 //#define BOOST_NO_CXX11_SCOPED_ENUMS
 #include <boost/filesystem.hpp>
@@ -13,29 +20,90 @@
 
 
 
+
+//TODO:
+/*
+
+- Create proejction matrix 
+cv::FileStorage fSettings(strSettingPath, cv::FileStorage::READ);
+
+    
+    float fx = fSettings["Camera.fx"];
+    float fy = fSettings["Camera.fy"];
+    float cx = fSettings["Camera.cx"];
+    float cy = fSettings["Camera.cy"];
+    float bf = fSettings["Camera.bf"];
+
+    cv::Mat projMatrl = (cv::Mat_<float>(3, 4) << fx, 0., cx, 0., 0., fy, cy, 0., 0,  0., 1., 0.);
+    cv::Mat projMatrr = (cv::Mat_<float>(3, 4) << fx, 0., cx, bf, 0., fy, cy, 0., 0,  0., 1., 0.);
+
+    use glob
+    use cv::decomposeProjectionMatrix to decompose P =  [K R t]
+    import gt in  a gt_matrix
+
+     Mat H = convertToHomogeneousMat(R_trans, T_trans);
+
+    Transformation Matrix is actually called Homogenous Transformation Matrix
+
+    Plot the predicted point and the acutal ground truth and the error in realtime
+
+    GNUPlot
+
+*/
+
 class VisualOdom{
+
+
+    private:
+
+        //**input functions
+        void read_projection_matrix();
+        void load_camera_params_matrix(); 
+        void load_camera_params();
+        void read_ground_truth_poses();
 
     public:     
 
         VisualOdom(const std::string &folder_);
 
-        void load_camera_params();
         
-
-
         void build_image_list(const std::string &folder_);
         void extract_features(const cv::Mat &img_1, const cv::Mat &img_2);
         void match_features(const cv::Mat &img_1, const cv::Mat &img_2);
+        cv::Mat get_essential_matrix(const cv::Mat &img_1, const cv::Mat &img_2);        
         
-    //private:
-
-        std::string image_folder_;
+        double getAbsoluteScale(int frame_id);
+    
+        std::string image_folder_;  
         
         std::vector<boost::filesystem::path> image_path_list_; 
         std::vector<cv::KeyPoint> kp_1, kp_2; 
-  
 
-};
+        
+        std::vector<cv::Point2f> kp_1_matched, kp_2_matched; 
+
+
+
+        double focal_ ; 
+        cv::Point2d pp_;
+
+        //** data vars
+        std::vector<cv::Mat> gt_poses_;
+        
+        
+        //** camera parameters -> extrinsic + intrinsic
+        cv::Mat P_; //camera projection matrix -> [3 * 3]
+        cv::Mat K_; //camera matrix -> [3 * 3]
+        cv::Mat R_; //camera rotation -> [3 * 3]
+        cv::Mat t_; //camera translation -> [4 * 1]
+
+        //** file input vars
+        std::string base_dir_; 
+        std::string data_dir_;
+        std::string calib_file_name_;
+        std::string gt_file_name_;
+
+};  
 
 
 #endif
--- src/visual_odom.cpp
+++ src/visual_odom.cpp
@@ -1,19 +1,186 @@
 #include "../include/visual_odom.hpp"
 #include <boost/foreach.hpp> 
 
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include <fstream>
+#include <iostream>
+
 
 VisualOdom::VisualOdom(const std::string &folder_):image_folder_{folder_}{
 
     std::cout << "Inside the VO constructor!" << std::endl;    //build_image_list(image_folder_);
 
-    build_image_list(folder_);
+    base_dir_ = "/home/skpro19/simple_visual_odom/";
+    data_dir_ = base_dir_ + "data/02/";
 
+    calib_file_name_ = "calib.txt";
+    gt_file_name_ = "02.txt";
+
+    read_projection_matrix();
+    read_ground_truth_poses();
+    //load_camera_params();
+
+
+    //build_image_list(folder_);
+
+    //load_camera_params();
 
 
 }
+
+void VisualOdom::read_ground_truth_poses(){
+
+    std::cout <<"Inside the read_ground_truth_poses function!" << std::endl;
+
+    std::string gt_file_ = data_dir_ + gt_file_name_;
+    
+    std::ifstream gt_;
+
+    gt_.open(gt_file_);
+    
+    if(gt_.is_open()) {
+
+        std::string line_;
+
+        while(std::getline(gt_, line_)){
+
+            std::cout << "line: " << line_ << std::endl;
+
+            double f_;
+
+            std::stringstream ss_(line_);
+            std::vector<float> v_; 
+
+            for(int i = 0; i < 12 ; i++) {
+
+                ss_ >> f_; 
+                v_.push_back(f_);
+
+            }
+            
+            std::cout << "v_.size(): " << v_.size() << std::endl; 
+
+            for(auto t: v_) std::cout << t << " " ;
+            std::cout << std::endl;
+
+            cv::Mat gt_;
+            gt_ = cv::Mat(v_).reshape(0, 3);
+            gt_.convertTo(gt_, CV_64F);
+
+            gt_poses_.push_back(gt_);
+
+        }
+        
+        //std::cout << P_.size() << " " << P_ << std::endl;
+
+    }
+
+
+
+    else {
+
+        std::cerr << "Unable to read calib file!" << std::endl;
+
+    }
+
+    //cv::FileStorage fs_(calib_file_, cv::FileStorage::READ);
+
+    
+
+
+}
+
+
+
+void VisualOdom::load_camera_params_matrix(){
+
+    cv::decomposeProjectionMatrix(P_, K_, R_, t_);
+
+    std::cout << "P_.size(): " << P_.size() << std::endl; 
+    std::cout << "K_.size(): " << K_.size() << std::endl; 
+    
+    std::cout << "K_: " << K_ << std::endl;
+    
+
+    std::cout << "R_.size(): " << R_.size() << std::endl;
+    std::cout << "R_: " << R_ << std::endl; 
+    
+    std::cout << "t_.size(): " << t_.size() << std::endl;
+    std::cout << "t_: " << t_ << std::endl;
+
+}
+
+
+/**
+ * @brief read projection matrix from calib.txt to P_
+ * 
+ */
+
+void VisualOdom::read_projection_matrix(){
+
+    std::string calib_file_ = data_dir_ + calib_file_name_;
+    
+    //std::cout << "calib_file_: " << calib_file_ << std::endl;
+
+    std::ifstream calib_;
+
+    calib_.open(calib_file_);
+    if(calib_.is_open()) {
+
+        std::string line_;
+        std::getline(calib_, line_);
+
+        //std::cout << "line_: " << line_ << std::endl;
+
+        std::string s_;
+        double f_;
+
+        std::stringstream ss_(line_);
+
+        ss_ >> s_; // bypass "P0:""
+
+        //std::cout << "s_: " << s_ << std::endl;
+
+        std::vector<float> v_;
+
+        int cnt_ = 12;
+        while(ss_ >> f_ && cnt_ > 0) {
+
+            v_.push_back(f_);
+            cnt_--;
+
+        }
+
+        //std::cout << "v_.size(): " << v_.size() << std::endl; 
+
+        for(auto t: v_) std::cout << t << " " ;
+        std::cout << std::endl;
+
+        //auto *ptr =  v_.data();
+
+        //P_ = cv::Mat(3, 4,  CV_64F, ptr);
+        
+        P_ = cv::Mat(v_).reshape(0, 3);
+        P_.convertTo(P_, CV_64F);
+
+        std::cout << P_.size() << " " << P_ << std::endl;
+
+    }
+
+
+
+    else {
+
+        std::cerr << "Unable to read calib file!" << std::endl;
+
+    }
+
+    //cv::FileStorage fs_(calib_file_, cv::FileStorage::READ);
+
+    
+
+
+}
+
 
 void VisualOdom::extract_features(const cv::Mat &img_1, const cv::Mat &img_2){
 
@@ -23,9 +190,8 @@ void VisualOdom::extract_features(const cv::Mat &img_1, const cv::Mat &img_2){
     cv::cvtColor(img_2, image_two, cv::COLOR_BGR2GRAY);
 
     std::vector< cv::Point2f > corners_one, corners_two;
-
     
-    int maxCorners = 5000;
+    int maxCorners = 2000;
 
     double qualityLevel = 0.01;
 
@@ -42,24 +208,22 @@ void VisualOdom::extract_features(const cv::Mat &img_1, const cv::Mat &img_2){
     
     //*** keypoints extraction
     cv::goodFeaturesToTrack( image_one, corners_one, maxCorners, qualityLevel, minDistance, mask, blockSize, useHarrisDetector, k );
+
     cv::goodFeaturesToTrack( image_two, corners_two, maxCorners, qualityLevel, minDistance, mask, blockSize, useHarrisDetector, k );
 
-    
-    for (auto point: corners_one) {
+    cv::KeyPoint::convert(kp_1, corners_one, std::vector<int>());
+    cv::KeyPoint::convert(kp_2, corners_two, std::vector<int>());
 
-        kp_1.push_back(cv::KeyPoint(cv::Point2f(point.x, point.y), 2));
-
-    }
-
-    for (auto point: corners_two) {
-
-
-        kp_2.push_back(cv::KeyPoint(cv::Point2f(point.x, point.y), 2));
-    }
-    
 
 }
 
+
+void VisualOdom::load_camera_params(){
+
+    focal_ = 718.85; 
+    pp_ = {607.19, 185.21};
+
+}
 
 void VisualOdom::match_features(const cv::Mat &img_1, const cv::Mat &img_2){
     
@@ -71,29 +235,24 @@ void VisualOdom::match_features(const cv::Mat &img_1, const cv::Mat &img_2){
     cv::Mat mask = cv::Mat();
     cv::Mat des_1, des_2;
     
-    cv::Ptr<cv::ORB>orb_ = cv::ORB::create(5000);
+    //cv::Ptr<cv::ORB>orb_ = cv::ORB::create(5000);
+    cv::Ptr<cv::ORB>orb_ = cv::ORB::create(2000);
 
     
-
-
+    
     //*** extracting descriptors from keypoints
     orb_->detectAndCompute(image_one, mask, kp_1, des_1);
     orb_->detectAndCompute(image_two, mask, kp_2, des_2);
     
-   // des_1.convertTo(des_1, 5);
-    //des_2.convertTo(des_2, 5);
     
     des_1.convertTo(des_1, 0);
     des_2.convertTo(des_2, 0);
     
-
-    //cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
     cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce-Hamming");
 
     std::vector<cv::DMatch> brute_hamming_matches;
     matcher->match(des_1, des_2, brute_hamming_matches);
 
-    
     double min_dist=10000, max_dist=0;
 
     for ( int i = 0; i < des_1.rows; i++ )
@@ -103,32 +262,23 @@ void VisualOdom::match_features(const cv::Mat &img_1, const cv::Mat &img_2){
         if ( dist > max_dist ) max_dist = dist;
     }
 
-    printf ( "-- Max dist : %f \n", max_dist );
-    printf ( "-- Min dist : %f \n", min_dist );
-
     std::vector<cv::DMatch> good_matches;
     
     for ( int i = 0; i < des_1.rows; i++ )
     {
         if ( brute_hamming_matches[i].distance <= std::max( 2*min_dist, 30.0 ) )
         {
-            good_matches.push_back (brute_hamming_matches[i] );
+            good_matches.push_back (brute_hamming_matches[i]);
         }
     }
-    
+
 
     for (auto match : good_matches) {
-
-        cv::circle( img_1, kp_1[match.queryIdx].pt, 2, cv::Scalar( 0, 255, 0), -1 );
-        cv::circle( img_1, kp_2[match.trainIdx].pt, 2, cv::Scalar( 255, 0, 0), -1 );
-        cv::line(img_1, kp_1[match.queryIdx].pt, kp_2[match.trainIdx].pt, cv::Scalar(0, 255,0));
+        
+        kp_1_matched.push_back(cv::Point2f{kp_1[match.queryIdx].pt.x, kp_1[match.queryIdx].pt.y}); 
+        kp_2_matched.push_back(cv::Point2f{kp_2[match.trainIdx].pt.x, kp_2[match.trainIdx].pt.y}); 
+        
     }
-
-    
-
-    cv::imshow("img",img_1);
-    cv::waitKey(10);
-  
 
 }
 
@@ -157,27 +307,148 @@ void VisualOdom::build_image_list(const std::string &folder){
 
 }
 
+double VisualOdom::getAbsoluteScale(int frame_id)	{
 
+    using namespace std;
+
+    string line;
+    int i = 0;
+    ifstream myfile ("/home/skpro19/simple_visual_odom/data/02/02.txt");
+    
+    double x =0, y=0, z = 0;
+    double x_prev, y_prev, z_prev;
+    if (myfile.is_open())
+    {
+    while (( getline (myfile,line) ) && (i<=frame_id))
+    {
+    z_prev = z;
+    x_prev = x;
+    y_prev = y;
+    std::istringstream in(line);
+    //cout << line << '\n';
+    for (int j=0; j<12; j++)  {
+    in >> z ;
+    if (j==7) y=z;
+    if (j==3)  x=z;
+    }
+
+    i++;
+    }
+    myfile.close();
+    }
+
+    else {
+    cout << "Unable to open file";
+    return 0;
+    }
+
+    return sqrt((x-x_prev)*(x-x_prev) + (y-y_prev)*(y-y_prev) + (z-z_prev)*(z-z_prev)) ;
+
+}
 
 
 
 
 int main(){
 
+    //VisualOdom::VisualOdom vo_("abcd");
 
     VisualOdom visual_odom_("../data/02/image_0/");
 
+    //std::cout << visual_odom_.gt_poses_[0].size() << " " << visual_odom_.gt_poses_[0] << std::endl;
+
+    //cv::Mat gt_1 = visual_odom_.gt_poses_[0]; 
+
+    /*std::cout << "gt_1.size(): " << gt_1.size() << std::endl;
+    std::cout << "gt_1: " << gt_1 << std::endl;
+
+
+    int cnt = 10; 
+    while(cnt--) std::cout << std::endl;
+    */
+
+    int sz_ = (int)visual_odom_.gt_poses_.size(); 
+    
+    int idx_ =0 ; 
+
+    while(idx_ < sz_ - 1) {
+
+        cv::Mat m_ = visual_odom_.gt_poses_[idx_];
+        std::cout << "(" << m_.at<float>(0,3) << "," << m_.at<float>(1,3) << ","  << m_.at<float>(2,3) << ")" << std::endl;
+        idx_++;
+        
+    }
+    
+    /*cv::Mat lpm = cv::Mat(3, 4, CV_32F);
+
+    std::cout << "lpm.size(): " << lpm.size() << std::endl; 
+
+    std::cout << "lpm--> " << lpm <<  std::endl;
+    */
+    /*VisualOdom visual_odom_("../data/02/image_0/");
+
     int sz_ = (int)visual_odom_.image_path_list_.size(); 
+
+   //sz_ = 2; 
+
+    //TODO: initialize with ground truth
+    
+    cv::Mat T_i, T_f, h_;  //transformation matrices
+
+    cv::Mat X_i, X_f; //camera pose in the world frame
+
+    T_i = (cv::Mat_<double>(4, 4) << 1, 0, 0, 0, 0, 1, 0 , 0, 0, 0 , 1, 0, 0 ,0 ,0 , 1); 
+    h_ = (cv::Mat_<double>(1,4) << 0 , 0, 0, 1); // needed to create the form T = [R| t]
+
+    X_i = (cv::Mat_<double>(4,1) << 0 , 0, 0 , 1);
+
+    */  
+
+  // int sz_ = (int)visual_odom_.image_path_list_.size();
+
+   std::cout << "sz_: " << sz_ << std::endl;
+
+    sz_ = 2; 
 
     for(int i = 0 ; i < sz_ - 1; i++) {
 
+        visual_odom_.kp_1.resize(0); 
+        visual_odom_.kp_2.resize(0); 
+
+        visual_odom_.kp_1_matched.resize(0); 
+        visual_odom_.kp_2_matched.resize(0);
+    
         cv::Mat img_1 = cv::imread(visual_odom_.image_path_list_[i].c_str());
         cv::Mat img_2 = cv::imread(visual_odom_.image_path_list_[i + 1].c_str());
 
         visual_odom_.extract_features(img_1, img_2);
         visual_odom_.match_features(img_1, img_2);
+        
+        cv::Mat E_, mask_, R, t;
+
+        //E_ = cv::findEssentialMat(visual_odom_.kp_1_matched, visual_odom_.kp_2_matched, visual_odom_.focal_, visual_odom_.pp_,cv::RANSAC, 0.99, 1.0, mask_);
+        E_ = cv::findEssentialMat(visual_odom_.kp_2_matched, visual_odom_.kp_1_matched, visual_odom_.focal_, visual_odom_.pp_,cv::RANSAC, 0.99, 1.0, mask_);
+        
+        //cv::recoverPose(E_, visual_odom_.kp_1_matched, visual_odom_.kp_2_matched, R ,t ,visual_odom_.focal_, visual_odom_.pp_, mask_);
+        cv::recoverPose(E_, visual_odom_.kp_2_matched, visual_odom_.kp_1_matched, R ,t ,visual_odom_.focal_, visual_odom_.pp_, mask_);
+
+        std::cout << "E_.size(): " << E_.size() << std::endl; 
+        std::cout << "E_: " << E_ << std::endl;
+        
+        std::cout << "R.size(): " << R.size() << std::endl;
+        std::cout << "R: " << R << std::endl;
+
+        std::cout << "t.size(): " << t.size() << std::endl;
+        std::cout << "t: " << t << std::endl;
+
+        cv::Mat Rt = R * t; 
+
+        std::cout <<"Rt.size(): " << Rt.size() << std::endl;
+        std::cout << "Rt: " << Rt << std::endl;
 
     }
+
+    
 
     return 0;
 
